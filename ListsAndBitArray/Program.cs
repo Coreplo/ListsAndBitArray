0fc718478efb1c3731a4d7704928b77a19620b45
Coreplo <coreplo@gmail.com>
2019-07-11 15:50:33 +0200

    lists in csharp

--- ListsAndBitArray/Program.cs
+++ ListsAndBitArray/Program.cs
@@ -10,12 +10,42 @@ namespace ListsAndBitArray
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lists");
+            // List is like an array but elements can be added and removed dynamically
+            // The Generic Collection List<T> requires all elments to be of the same type T
+
+            // Some List<T> methods: 
+
+            List<int> li = new List<int>();
+
+            li.Add(56);
+            li.Add(2);
+            li.Add(1);
+            li.Add(86);
+            li.Add(6);
+            li.Add(26);
+
+            PrintList(li);
+
+            li.RemoveAt(1);
+
+            PrintList(li);
+
+            li.Sort();
+
+            PrintList(li);
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+        static void PrintList(List<int> myList) {
+            Console.Write("\n List: ");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.Write(myList[i] + " ");
+            }
         }
     }
 }
